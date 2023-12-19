f7cac52fa8db2739e51b769af5a61e9cba92ac83
Souleymane Sall <papa.sall243@gmail.com>
2023-12-19 15:33:05 +0100

    Update functions using methods ✔

--- main.go
+++ main.go
@@ -4,36 +4,43 @@ import (
 	"fmt"
 )
 
-func get(m map[string]string, mot string){
-	_, prs := m[mot]
+type crud struct {
+    mot string
+	m map[string]string
+}
+
+func (c crud) get(){
+	_, prs := c.m[c.mot]
 	if prs == false {
 		fmt.Println("Mot inexistant")
 	}else {
-		fmt.Println(m[mot])
+		fmt.Println("Mot trouve")
+		fmt.Println(c.m[c.mot])
 	}
 }
 
-func list(m map[string]string){
-	fmt.Println("Map list:", m)
+func (c crud) list(){
+	fmt.Println("Map list:", c.m)
 }
 
-func remove(m map[string]string, mot string){
-	delete(m, mot)
-	fmt.Println(mot + " suprimme avec succes")
+func (c *crud) remove(){
+	delete(c.m, c.mot)
+	fmt.Println(c.mot + " suprimme avec succes")
 }
 
 func main() {
 
-    m := make(map[string]string)
-
-    m["mot"] = "Chacun des sons ou groupes de sons (de lettres ou groupes de lettres) correspondant à un sens isolable spontanément, dans le langage ; (par écrit) suite ininterrompue de lettres, entre deux blancs"
-    m["rétrospective"] = "Exposition, manifestation qui présente les œuvres et l'évolution d'un artiste, d'une école."
+	c := crud{ mot: "mot", m : make(map[string]string)}
 
 
+    c.m["mot"] = "Chacun des sons ou groupes de sons (de lettres ou groupes de lettres) correspondant à un sens isolable spontanément, dans le langage ; (par écrit) suite ininterrompue de lettres, entre deux blancs"
+    c.m["rétrospective"] = "Exposition, manifestation qui présente les œuvres et l'évolution d'un artiste, d'une école."
 
-	get(m, "mot")
-	list(m)
-	remove(m, "mot")
-	get(m, "mot")
+
+
+	c.get()
+	c.list()
+	c.remove()
+	c.get()
     
 }
