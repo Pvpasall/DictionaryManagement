dbc40c7f3af803b1c145d7533770e4eb4403c8a3
Souleymane Sall <papa.sall243@gmail.com>
2023-12-19 12:49:38 +0100

    Read and Delete

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main()  {
-	fmt.Println("My Dico Project")
+func get(m map[string]string, mot string){
+	_, prs := m[mot]
+	if prs == false {
+		fmt.Println("Mot inexistant")
+	}else {
+		fmt.Println(m[mot])
+	}
+}
+
+func list(m map[string]string){
+	fmt.Println("Map list:", m)
+}
+
+func remove(m map[string]string, mot string){
+	delete(m, mot)
+	fmt.Println(mot + " suprimme avec succes")
+}
+
+func main() {
+
+    m := make(map[string]string)
+
+    m["mot"] = "Chacun des sons ou groupes de sons (de lettres ou groupes de lettres) correspondant à un sens isolable spontanément, dans le langage ; (par écrit) suite ininterrompue de lettres, entre deux blancs"
+    m["rétrospective"] = "Exposition, manifestation qui présente les œuvres et l'évolution d'un artiste, d'une école."
+
+
+
+	get(m, "mot")
+	list(m)
+	remove(m, "mot")
+	get(m, "mot")
+    
 }
