414f2f807936ad82bcff1cbd5e1cd2d509ebcb02
Souleymane Sall <papa.sall243@gmail.com>
2023-12-20 11:54:30 +0100

    Gestion de Données avec Fichier done ✔ with json file. After a long journey �

--- dictionnary/dictionary.go
+++ dictionnary/dictionary.go
@@ -1,40 +1,72 @@
 package dictionnary
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"os"
 )
 
-type Crud struct {
-	m map[string]string
+type Dico struct {
+	Mot        string `json:"mot"`
+	Definition string `json:"definition"`
 }
 
-func New() Crud{
-	c := Crud{m: make(map[string]string)}
-	return c
+type CRUD struct {
+	filePath string
 }
 
-func (c *Crud) Add(mot string, definition string) {
-    if c.m == nil {
-        c.m = make(map[string]string)
-    }
-    c.m[mot] = definition
+func New(filePath string) CRUD {
+	return CRUD{filePath: filePath}
 }
 
-func (c Crud) Get(mot string){
-	_, prs := c.m[mot]
-	if prs == false {
-		fmt.Println("Mot inexistant")
-	}else {
-		fmt.Println("Mot trouve")
-		fmt.Println(c.m[mot])
+func (c CRUD) Add(mot, definition string) string {
+	entry := Dico{Mot: mot, Definition: definition}
+	entries := c.readDico()
+	entries = append(entries, entry)
+	c.writeDico(entries)
+
+	return mot + " et sadefinition ajouter avec succes !"
+}
+
+func (c CRUD) Get(mot string) string {
+	entries := c.readDico()
+	for _, entry := range entries {
+		if entry.Mot == mot {
+			return entry.Definition
+		}
 	}
+	return mot + " introuvable"
 }
 
-func (c Crud) List(){
-	fmt.Println("Map list:", c.m)
+func (c CRUD) List() []Dico {
+	return c.readDico()
 }
 
-func (c *Crud) Remove(mot string){
-	delete(c.m, mot)
-	fmt.Println(mot + " suprimme avec succes")
+func (c CRUD) Remove(mot string) string {
+	entries := c.readDico()
+	var updatedDico []Dico
+	for _, entry := range entries {
+		if entry.Mot != mot {
+			updatedDico = append(updatedDico, entry)
+		}
+	}
+	c.writeDico(updatedDico)
+
+	return mot + " et sa definition supprime avec succes"
+}
+
+func (c CRUD) readDico() []Dico {
+	file, _ := os.Open(c.filePath)
+	defer file.Close()
+	byteValue, _ := ioutil.ReadAll(file)
+	var entries []Dico
+	_ = json.Unmarshal(byteValue, &entries)
+	return entries
+}
+
+func (c CRUD) writeDico(entries []Dico) {
+	file, _ := os.OpenFile(c.filePath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	defer file.Close()
+	byteValue, _ := json.Marshal(entries)
+	_, _ = file.Write(byteValue)
 }
--- main.go
+++ main.go
@@ -6,9 +6,10 @@ import (
 
 func main() {
 
-	dict := dictionnary.New()
+	dict := dictionnary.New("dictionnary.json")
 
 	dict.Add("mot", "Chacun des sons ou groupes de sons (de lettres ou groupes de lettres) correspondant à un sens isolable spontanément, dans le langage ; (par écrit) suite ininterrompue de lettres, entre deux blancs")
+	dict.Add("mot2", "Chacun des sons ou groupes de sons (de lettres ou groupes de lettres) correspondant à un sens isolable spontanément, dans le langage ; (par écrit) suite ininterrompue de lettres, entre deux blancs")
 	dict.Add("rétrospective", "Exposition, manifestation qui présente les œuvres et l'évolution d'un artiste, d'une école.")
 
 	dict.Get("mot")
